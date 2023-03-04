bbf4e7a56f7a484eed7517f9a864552dfd3d5e50
Dominique Peytrignet <dominique.peytrignet@hotmail.com>
2023-03-04 15:59:50 +0100

    new_images

--- PythonFiles/object_size.py
+++ PythonFiles/object_size.py
@@ -105,12 +105,12 @@ for c in cnts:
 	dimB = dB / pixelsPerMetric
 
 	# draw the object sizes on the image
-	cv2.putText(orig, "{:.1f}cm".format(dimA),
-		(int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (255, 255, 255), 2)
 	cv2.putText(orig, "{:.1f}cm".format(dimB),
-		(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (255, 255, 255), 2)
+		(int(tltrX - 15), int(tltrY)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+	cv2.putText(orig, "{:.1f}cm".format(dimA),
+		(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+	
+	
 
 	# show the output image
 	cv2.imshow("Image", orig)
